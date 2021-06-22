8f165f1c06fd6b90c11b14c0d7202947d5c31083
guswn2521 <61112694+guswn2521@users.noreply.github.com>
2021-06-22 23:24:01 +0900

    #2.1 2D Context
    1. canvas.getContext("2d")로 이차원 컨텍스트 객체를 생성
    2. 객체의 초기 strokeStyle : 색깔, lineWidth : 굵기를 설정.
    3. startPainting()함수 생성
    4. onMouseMove함수에서 마우스가 떼져있으므로 painting=false임.
    -> beginPath() & moveTo(x,y) : !painting일 때 캔버스의 context객체가 마우스의 경로를 추적함
    마우스를 클릭했을 때 lineTo() & stroke()로 선을 그리려 시도함.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
 
@@ -6,23 +11,34 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = ture;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
+// function onMouseUp(event){
+//     stopPainting();
+// }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 
 }
